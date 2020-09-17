acf7801e06910135eb0d365d583c70c0b767c5ee
Alan Weatherhead <alanwea@hotmail.com>
2020-09-17 13:42:55 -0700

    In classifier.py permission error- passing in the folder name of images, where is it getting name from?  Should it be file or folder name?

--- check_images.py
+++ check_images.py
@@ -82,7 +82,7 @@ def main():
     #             classify_images(in_arg.dir, results, in_arg.arch)
     # Creates Classifier Labels with classifier function, Compares Labels,
     # and adds these results to the results dictionary - results
-    classify_images(None, results, None)
+    classify_images(in_arg.dir, results, 'vgg')
 
     # Function that checks Results Dictionary using results
     check_classifying_images(results)
